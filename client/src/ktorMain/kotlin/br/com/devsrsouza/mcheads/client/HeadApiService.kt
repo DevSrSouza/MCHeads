9582b3a0b560cf9ae9afd033af2e55051773146e
DevSrSouza <devsrsouza@gmail.com>
2019-09-14 13:32:21 -0300

    ktor client kotlinx serialization support and better implementation

--- client/src/ktorMain/kotlin/br/com/devsrsouza/mcheads/client/HeadApiService.kt
+++ client/src/ktorMain/kotlin/br/com/devsrsouza/mcheads/client/HeadApiService.kt
@@ -3,33 +3,35 @@ package br.com.devsrsouza.mcheads.client
 import br.com.devsrsouza.mcheads.common.Head
 import br.com.devsrsouza.mcheads.common.HeadCategory
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.list
+import io.ktor.client.response.HttpResponse
+import io.ktor.http.HttpStatusCode
+import io.ktor.util.toByteArray
 
 class KtorHeadApi(private val http: HttpClient) : IHeadApiService {
 
     override suspend fun search(
         name: String
     ): List<Head> {
-        return parseHeadJsonList(http.get("${API_URL}/search/${name}"))
+        return http.get("${API_URL}/search/${name}")
     }
 
     override suspend fun searchCategory(
         category: HeadCategory
     ): List<Head> {
-        return parseHeadJsonList(http.get("${API_URL}/category/${category.name.toLowerCase()}"))
-    }
-
-    private fun parseHeadJsonList(json: String): List<Head> {
-        return Json.parse(Head.serializer().list, json)
+        return http.get("${API_URL}/category/${category.name.toLowerCase()}")
     }
 
     override suspend fun getRender(
         headId: Int
     ): ByteArray? {
         try {
-            return http.get<ByteArray>("${API_URL}/image/${headId}")
+            val response = http.get<HttpResponse>("${API_URL}/image/${headId}")
+            if(response.status == HttpStatusCode.OK) {
+                return response.content.toByteArray()
+            } else return null
         } catch (e: Throwable) {
             return null
         }
@@ -39,6 +41,10 @@ class KtorHeadApi(private val http: HttpClient) : IHeadApiService {
 actual object HeadApi {
 
     actual val service: IHeadApiService by lazy {
-        KtorHeadApi(HttpClient())
+        KtorHeadApi(HttpClient(){
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+        })
     }
 }
